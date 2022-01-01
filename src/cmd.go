de33aef25ddcb9531288ebc0cca9157c9ca56412
Dengxinlin <534442429@qq.com>
2022-01-01 13:05:06 +0800

    Initial commit, add args parse

---
+++ src/cmd.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+//
+//  Cmd
+//  @Description: struct for cmd parse
+//
+type Cmd struct {
+	helpFlag    bool
+	versionFlag bool
+	// classpath
+	cpOption string
+	// java bytecode file path
+	class string
+	args  []string
+}
+
+//
+//	parseCmd
+//	@Description: cmd parse
+//	@return *Cmd
+//
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+	return cmd
+}
+
+//
+//	printUsage
+//	@Description: print the usage of jvm
+//
+func printUsage() {
+	fmt.Printf("Usage: %s [-option] class [args...]\n", os.Args[0])
+}
---
+++ src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	cmd := parseCmd()
+	if cmd.versionFlag {
+		fmt.Printf("version 0.0.1")
+	} else if cmd.helpFlag || cmd.class == "" {
+		printUsage()
+	} else {
+		startJVM(cmd)
+	}
+}
+
+//
+//	startJVM
+//	@Description: start the jvm
+//	@param cmd *Cmd
+//
+func startJVM(cmd *Cmd) {
+	fmt.Printf("classpath:%s class:%s args:%v\n",
+		cmd.cpOption, cmd.class, cmd.args)
+}
