76c5cc510c243bb98f211bf8d674a0e4b2eea0c2
linxin2429 <dengxinlin2429@gmail.com>
2022-01-10 23:32:51 +0800

    add registry small change test

--- src/native/registry.go
+++ src/native/registry.go
@@ -5,3 +5,8 @@ import "jvm/src/rtda"
 type NativeMethod func(frame *rtda.Frame)
 
 var registry = map[string]NativeMethod{}
+
+func Register(className, methodName, methodDescriptor string, method NativeMethod) {
+	key := className + "~" + methodName + "~" + methodDescriptor
+	registry[key] = method
+}
